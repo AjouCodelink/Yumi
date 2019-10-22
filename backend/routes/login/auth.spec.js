8cb293866c4acee73adff12ed79ae844874d40cc
sanghyun <tkdgusl94@naver.com>
2019-10-22 16:13:10 +0900

    unit test 테스트 코드 작성

---
+++ backend/routes/login/auth.spec.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const should = require('should');
+const app = require('./auth');
+
+describe('GET /users', () => {
+  it('should return 200 status code', () => {
+    (true).should.be.equal(true);
+  });
+});
