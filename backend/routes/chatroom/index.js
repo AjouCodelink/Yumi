e6955edd2bcc8b45ed5c52759c423e4f7578c0ce
tkdgusl94 <tkdgusl94@naver.com>
2019-11-04 15:20:35 +0900

    creation.js init

--- backend/routes/chatroom/index.js
+++ backend/routes/chatroom/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var searchWord = require('./searchWord');
+var creation = require('./creation');
 
 router.use('/search', searchWord);
+router.use('/creation', creation);
 
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'chatroom' });
