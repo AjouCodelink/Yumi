4ef0a840f126df3bca63d98378ca7d28723a4543
tkdgusl94 <tkdgusl94@naver.com>
2019-11-04 13:10:35 +0900

    io.js 모듈화

--- backend/routes/chatroom/index.js
+++ backend/routes/chatroom/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var searchWord = require('./searchWord');
-var chat = require('./chat');
 
 router.use('/search', searchWord);
-router.use('/chat', chat);
 
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'chatroom' });
