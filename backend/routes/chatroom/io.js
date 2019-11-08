dc25a6b13e3454c08f9d1555e7a54364d340fcca
tkdgusl94 <tkdgusl94@naver.com>
2019-11-08 11:31:15 +0900

    방 생성 api, 팝퀴즈 생성 로직 구현

--- backend/routes/chatroom/io.js
+++ backend/routes/chatroom/io.js
@@ -1,5 +1,6 @@
 module.exports = function (server) {
     var io = require('socket.io')(server);
+    var PopQuiz = require('../../models/popQuiz');
 
     io.on('connection', function (socket) {
         console.log(socket.id);
@@ -10,5 +11,21 @@ module.exports = function (server) {
             //socket.broadcast.emit('OTHER_MESSAGE', data); // 본인을 제외한 다른 사람들에게만 메세지 전송함
         });
     });
+
+    setInterval(() => { // 일정 주기로 팝퀴즈를 반환함
+        var data = {};
+
+        PopQuiz.find({}, function(err, datas){ 
+            if(err) return err;
+            var randomQuizNumber = Math.floor(Math.random() * datas.length); // 팝퀴즈 중 하나 랜덤으로 찾음
+
+            data.author = datas[randomQuizNumber].question; // TODO : 이거 나중에 프론트엔드에 RECEIVE_QUIZ 작성 되면 author -> question으로 수정
+            data.message = datas[randomQuizNumber].answer;
+
+            console.log(data);
+            io.emit('RECEIVE_MESSAGE', data); // TODO : RECEIVE_QUIZ로 나중에 수정 예정
+        })
+    }, 2000); // 시간 주기 설정 (2000 -> 2초)
+
     return io;
 }
