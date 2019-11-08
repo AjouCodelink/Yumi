5c0892fec632e9a562779ac041bfaa90d77f6455
LEEYOONGU <try3357@ajou.ac.kr>
2019-11-08 11:40:14 +0900

    UI Height 절대값, 우측 searchUI구현

--- frontend/Components/AppTabNavigator/ChatManageTab.js
+++ frontend/Components/AppTabNavigator/ChatManageTab.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, StyleSheet,TouchableOpacity,Text} from 'react-native';
-import { Container, Header, Item, Input, Button, Icon } from 'native-base';
+import { View, StyleSheet,TouchableOpacity,Text, TextInput} from 'react-native';
+import { Container, Header, Item, InputGroup, Input, Button, Icon, Right } from 'native-base';
 import {SearchBar} from 'react-native-elements'
 import LottieView from 'lottie-react-native';
 import { Animated, Easing } from 'react-native';
@@ -13,6 +13,10 @@ export default class ChatManageTab extends Component {
     updateSearch = search =>{
         this.setState({search});
     };
+    ButtonClickCheckFunction = () =>{
+
+    alert("Button Clicked");
+}
     constructor(props) {
         super(props);
         this.state = {
@@ -24,21 +28,27 @@ export default class ChatManageTab extends Component {
             <Icon name='md-add-circle' size={300} style={{color: tintColor}} />
         ),
     }
-
+    
     render() {
         const {search} = this.state;
         return (
             <View style={style.container}>
-                {/*<View style={style.header}>
+            
+                <View style = {style.search}>
+                <TextInput
+                        style={{height: 40, width: 300, backgroundColor:'#666',  fontSize:18, borderRadius: 5,paddingLeft: 10}}
+                        placeholder="Search..."
+                        value={this.state.search}
+                        onChangeText={(search) => this.setState({search})}
+                />
+                <Button 
+                style={{backgroundColor: '#384850',width: 50, height: 40, marginLeft :15, backgroundColor:'#AAA',}} 
+                onPress={ this.ButtonClickCheckFunction}
+                >
+                <Icon name='ios-search' style={{color: '#FFF'}}/>
+                </Button>     
                 </View>
-                */}
                 <View style={style.content}>
-                <View style={style.search}>
-                <SearchBar
-                    placeholder="Type Here..."
-                    onChangeText={this.updateSearch}
-                    value={search}
-                    />
                 </View>
                     <View style ={style.buttongroup}>
                     <LottieView  source={require('../LottieAnimation/8874-cat.json')} autoplay loop/>
@@ -86,29 +96,37 @@ export default class ChatManageTab extends Component {
                         </View>
                     </View>
             </View>
-            </View>
-        
-
         );
     }
 }
 
 const style = StyleSheet.create({
+
     header: {
         width:'100%',
-        height:'17%',
-        justifyContent: 'flex-end',
-        paddingLeft: '4%',
-        paddingBottom: '1.1%',
+        height:'15%',
+        justifyContent: 'center',
         backgroundColor: '#333',
     },
-    
     search: {
+        paddingTop: 50,
+        paddingLeft : 20,
+        flexDirection: 'row',
         width:'100%',
         height:'20%',
-        justifyContent: "center",
         backgroundColor: '#333',
-        
+    },
+    SubmitButtonStyle: {
+        justifyContent: "center",
+        alignItems : "center",
+        width: 50,
+        height: 40,
+        backgroundColor:'#fff',
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#fff',
+        fontSize : 9,
+        marginLeft :15,
     },
     container: {
         flex: 1,
@@ -145,36 +163,36 @@ const style = StyleSheet.create({
     },
     button1: {
         width:'20%',
-        height:'15%',
+        height: 20,
         marginRight: '15%',
         marginTop: '10%',
         paddingTop: '15%',
     },
     button2: {
         width:'20%',
-        height:'15%',
+        height: 20,
         marginTop: '10%',
         paddingTop: '15%',
     },
     button3: {
         width:'20%',
-        height:'15%',
+        height: 25,
         marginRight: '25%',
     },
     button4: {
         width:'20%',
-        height:'15%',
+        height: 25,
     },
     button5: {
         width:'20%',
-        height:'10%',
+        height: 15,
         marginRight: '15%',
         marginBottom: '15%',
         paddingBottom: '20%',
     },
     button6: {
         width:'20%',
-        height:'10%',
+        height:     15,
         marginBottom: '15%',
         paddingBottom: '20%',
     },
