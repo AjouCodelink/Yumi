7f5afeaf3073f762aac1bcb6e2da0c0d89ed4c42
tkdgusl94 <tkdgusl94@naver.com>
2019-11-08 15:24:48 +0900

    place/index.js init

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,14 @@ var router = express.Router();
 var login = require('./login/index');
 var signup = require('./signup/index');
 var chatroom = require('./chatroom/index');
+var place = require('./place/index');
 const passport = require('passport');
 
 
 router.use('/login', login);
 router.use('/signup', signup);
 router.use('/chatroom', chatroom);
+router.use('/place', place);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
