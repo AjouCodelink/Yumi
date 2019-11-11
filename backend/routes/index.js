7f615e201b19a64ff66dfb05b4bbf939508efb54
tkdgusl94 <tkdgusl94@naver.com>
2019-11-11 20:01:33 +0900

    middlewares/auth 설정 수정

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,7 @@ var router = require('express').Router();
 var user = require('./user/index');
 var chatroom = require('./chatroom/index');
 var place = require('./place/index');
-var authMiddleware = require('../middlewares/auth');
 
-router.use('/', authMiddleware);
 router.use('/user', user);
 router.use('/chatroom', chatroom);
 router.use('/place', place);
