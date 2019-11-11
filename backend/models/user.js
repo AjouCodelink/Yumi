44dbce73b789862219c93ca3bfe209a83a457c14
tkdgusl94 <tkdgusl94@naver.com>
2019-11-11 13:15:36 +0900

    JWT) user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+const crypto = require('crypto')
+const config = require('../config')
 
-const UserSchema = mongoose.Schema({
+const UserSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, trim: true },
-    nickname: { type: String, required: true },
+    //nickname: { type: String, required: true },
     interests: [{
         section: { type: String },
         group: { type: String }
@@ -21,5 +23,24 @@ UserSchema.methods.comparePassword = function (inputPassword, cb) {
     }
 };
 
+// create new User document
+UserSchema.statics.create = function(email, password) {
+    const encrypted = crypto.createHmac('sha1', config.secret)
+                      .update(password)
+                      .digest('base64');
+    const user = new this({
+        email,
+        password: encrypted
+    })
+    // return the Promise
+    return user.save()
+}
+
+// find one user by using username
+UserSchema.statics.findOneByEmail = function(email) {
+    return this.findOne({
+        email
+    }).exec()
+}
 
 module.exports = mongoose.model('user', UserSchema);
