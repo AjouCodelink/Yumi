01c075238e232f9a2a8c2ca966a46a5a656324bf
LEEYOONGU <try3357@ajou.ac.kr>
2019-12-04 14:55:34 +0900

    관리자_서포터즈페이지완성

--- backend/models/supporters.js
+++ backend/models/supporters.js
@@ -6,7 +6,30 @@ const SupportersSchema = mongoose.Schema({
     email: { type: String },
     contact: { type: String},
     text: { type: String},
-    photo_path: { type: String}
+    photo_path: { type: String} //TODO: 프론트에서 서포터리스트에서 accept 하면 DB에 넣는 Flag 변수인 Boolean type 을 추가
 });
+
+// create new User document
+SupportersSchema.statics.create = function(supporter_info) {
+    const {
+        name,
+        email,
+        contact,
+        text,
+        photo_path
+    } = supporter_info;
+
+
+    const supporter = new this({
+        name,
+        email,
+        contact,
+        text,
+        photo_path
+    })
+    // return the Promise
+    return supporter.save()
+}
+
 
 module.exports = mongoose.model('supporter', SupportersSchema);
