061dbe32d9f8ba5008a02ae048e949dacb707516
LEEYOOONGU <44609477+LEEYOOONGU@users.noreply.github.com>
2019-12-05 19:34:30 +0900

    관리자 페이지 API 구현 완료 (#91)
    * 관리자 프론트엔드 서버 연동 완료
    
    * 관리자페이지 API구현 완성
    
    * img_path,question.id를 question.email로 변경
    
    * 추가 변경

--- backend/routes/supporter/index.js
+++ backend/routes/supporter/index.js
@@ -2,11 +2,13 @@ var router = require('express').Router();
 var controller = require('./supporter.controller');
 var authMiddleware = require('../../middlewares/auth');
 
+//router.post('/login/check', controller.login);
+router.get('/',controller.getMain);
+router.post('/assign', controller.assign);
+router.post('/accept',controller.accept); // 서포터 등록을 승인 
+router.get('/question',controller.getQuestion); // 관리자페이지에서 문의사항 리스트 받을때
+router.post('/questionFinished',controller.finish); // 관리자페이지에서 문의사항 해결하고 종료할떄
+router.post('/appendQuestion',controller.appendQuestion); // 유미에서 Question을 append 할때
 
-
-router.get('/supporter',controller.getMain);
-
-router.post('/login/check', controller.login);
-router.post('/supporter/assign', controller.assign);
 
 module.exports = router;
--- backend/routes/supporter/supporter.controller.js
+++ backend/routes/supporter/supporter.controller.js
@@ -2,103 +2,71 @@ var router = require('express').Router();
 const jwt = require('jsonwebtoken');
 var User = require('../../models/user');
 var Supporter = require('../../models/supporters');
+var Question = require('../../models/question');
+
 /*
     /api/supporter
 */
+
 exports.getMain = (req, res) => {
     Supporter.find({}, function(err, supporters){
         if(err) res.json({result: false, message: "not found supporters"})
         res.json(supporters);
     })
 }
+exports.assign = (req, res) => {
+    const supporter_info = req.body;
+    
+    Supporter.create(supporter_info)
+    .then(result => {
+        if(result) res.json({result:true});
+        else res.json({result:false});
+    });
+}
 
-/*
+exports.accept = (req,res) =>{
+    var email = req.body.email;
+    Supporter.findOne({email:email}, function(err, supporter){
+        if(err) res.json({result:0})
+        console.log(supporter);
+        supporter.isAccepted = true;
+        supporter.save();
+        res.json({result :1});
+    });
+} // 유미에서는 accepted=true 인 리스트만 가져올 것이다. 
+
+exports.getQuestion = (req,res) =>{
+        Question.find({}, function(err, questions){
+        if(err) res.json({result: false, message: "not found questions"})
+        res.json(questions);
+    })
+
+}
+exports.appendQuestion = (req,res) =>{
+
+    const Question_info = req.body;
+    Question.create(Question_info)
+    .then(result => {
+        if(result) res.json({result:true});
+        else res.json({result:1});
+    });
+}
+exports.finish = (req,res) =>{
+
+    const Question_info = req.body.email;
+    Question.remove({ email:Question_info }, function(err, output){
+        if(err) return res.status(500).json({ error: "Deletion Success"});
+        res.status(204).end();
+    })
+
+}
+    /*
     POST /api/login/checkLogin
     {
         email,
         password
     }
 */
-exports.login = (req, res) => {
-    const {email, password} = req.body
-    const secret = req.app.get('jwt-secret')
-
-    // check the user info & generate the jwt
-    const check = (user) => {
-        if(!user) {
-            // user does not exist
-            throw new Error('login failed')
-        } else {
-            // user exists, check the password
-            if(user.verify(password)) {
-                // create a promise that generates jwt asynchronously
-                const p = new Promise((resolve, reject) => {
-                    jwt.sign(
-                        {
-                            _id: user._id,
-                            email: user.email,
-                            admin: user.admin
-                        }, 
-                        secret, 
-                        {
-                            expiresIn: '7d',
-                            issuer: 'codelink.com',
-                            subject: 'userInfo'
-                        }, (err, token) => {
-                            if (err) reject(err)
-                            resolve(token) 
-                        })
-                })
-                return p
-            } else {
-                throw new Error('login failed')
-            }
-        }
-    }
-
-    // respond the token 
-    const respond = (token) => {
-        User.findOne({email:email},{email:1, nickname:1}, function(err, user){
-            if(err) res.json(err);
-            
-            res.json({
-                result:1,
-                message: 'logged in successfully',
-                userInfo: user,
-                token            
-            })
-        })
-    }
-
-    // error occured
-    const onError = (error) => {
-        res.status(403).json({
-            message: error.message,
-            result:0
-        })
-    }
-
-    // find the user
-    User.findOneByEmail(email)
-    .then(check)
-    .then(respond)
-    .catch(onError)
-
-}
-
-
-/*
-    GET /api/supporter/info
-*/
-exports.info = function(req, res){
-    var email = req.decoded.email;
-
-    User.findOne({email:email},{email:1, nickname:1, interests:1, language:1, address:1}, function(err, user){
-        res.json(user);
-    })
-}
-
-
 /*
     POST /api/supporter/signup
     {
@@ -110,12 +78,77 @@ exports.info = function(req, res){
     }
 */
 
-exports.assign = (req, res) => {
-    const supporter_info = req.body;
+// exports.login = (req, res) => {
+//     const {email, password} = req.body
+//     const secret = req.app.get('jwt-secret')
 
-    Supporter.create(supporter_info)
-    .then(result => {
-        if(result) res.json({result:true});
-        else res.json({result:false});
-    });
-}
+//     // check the user info & generate the jwt
+//     const check = (user) => {
+//         if(!user) {
+//             // user does not exist
+//             throw new Error('login failed')
+//         } else {
+//             // user exists, check the password
+//             if(user.verify(password)) {
+//                 // create a promise that generates jwt asynchronously
+//                 const p = new Promise((resolve, reject) => {
+//                     jwt.sign(
+//                         {
+//                             _id: user._id,
+//                             email: user.email,
+//                             admin: user.admin
+//                         }, 
+//                         secret, 
+//                         {
+//                             expiresIn: '7d',
+//                             issuer: 'codelink.com',
+//                             subject: 'userInfo'
+//                         }, (err, token) => {
+//                             if (err) reject(err)
+//                             resolve(token) 
+//                         })
+//                 })
+//                 return p
+//             } else {
+//                 throw new Error('login failed')
+//             }
+//         }
+//     }
+//     // respond the token 
+//     const respond = (token) => {
+//         User.findOne({email:email},{email:1, nickname:1}, function(err, user){
+//             if(err) res.json(err);
+            
+//             res.json({
+//                 result:1,
+//                 message: 'logged in successfully',
+//                 userInfo: user,
+//                 token            
+//             })
+//         })
+//     }
+
+//     // error occured
+//     const onError = (error) => {
+//         res.status(403).json({
+//             message: error.message,
+//             result:0
+//         })
+//     }
+
+//     // find the user
+//     User.findOneByEmail(email)
+//     .then(check)
+//     .then(respond)
+//     .catch(onError)
+// }
+// /*
+//     GET /api/supporter/info
+// */
+// exports.info = function(req, res){
+//     var email = req.decoded.email;
+//     User.findOne({email:email},{email:1, nickname:1, interests:1, language:1, address:1}, function(err, user){
+//         res.json(user);
+//     })
+// }
+
