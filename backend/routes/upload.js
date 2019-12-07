a93b08ec653dcea17f8ac02977c22dc7df2813e6
tkdgusl94 <46221990+tkdgusl94@users.noreply.github.com>
2019-12-07 14:54:59 +0900

    사진업로드 api & 서포터즈 정보 받는 api (#98)
    * 채팅방에서 사진 전송 안정화
    
    * 프로필 사진 변경 시 chatroom.participants.img_path도 변경
    
    * saveImage 함수명 수정
    
    * saveImage 함수 수정
    
    * POST /images/upload 작성
    
    * POST /images/upload 코드 수정
    
    * 서포터즈 사진 업로드 예정
    
    * supporter 정보 저장시 img_path도 저장

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const multer = require('multer');
 const gm = require('gm');
 const User = require('../models/user');
+const ChatRoom = require('../models/chatRoom');
+const Supporter = require('../models/supporters');
 const authMiddleware = require('../middlewares/auth');
 
-router.use('/', authMiddleware);
+router.use('/profile', authMiddleware);
 
 var _storage = multer.diskStorage({
     destination: function(req,file,cb) {
@@ -21,25 +23,74 @@ router.get('/upload', function(req, res){
     res.render('upload');
 })
 
-
+/*
+*   POST /images/profile
+*   {
+*       file
+*   }
+*/
 router.post('/profile', upload.single('file'), function (req, res) {
     var email = req.decoded.email;
 
     User.findOne({email: email}, function(err, user){ // User를 찾아서 새로 받은 사진 정보를 DB에 저장
         if(err) res.json({result:0, message : "not found user"})
         user.img_path = req.file.filename;
-
+        
         user.save(function(err, result){
             if(err) res.json({result:0, message: "not save image"});
 
+            var chatrooms = user.chatroom;
+            saveImage(user);
             res.json(req.file);
         })
     })
 })
 
 
-router.post('/supporter/upload', upload.single('file'), function (req, res) {
+function saveImage(user){
+    var chatrooms = user.chatroom;
+
+    chatrooms.map((chatroom) => {
+        ChatRoom.findOne({_id: chatroom.cr_id}, {participants: 1}, (err, room) => {
+            room.participants.map((participant) => {
+                if(participant.email == user.email){
+                    participant.img_path = user.img_path;
+                    room.save();
+                    return;
+                }
+            })
+        })
+    })
+}
+
+/*
+*   POST /images/upload
+    {
+        file
+    }
+*/
+router.post('/upload', upload.single('file'), function (req, res) {
+    console.log(req.file);
     res.json(req.file);
+})
+
+/*
+*   POST /images/supporter/upload
+    {
+        file
+    }
+ */
+router.post('/supporter/upload', upload.single('file'), function (req, res) {
+    var email = req.body.email;
+    var filename = req.file.filename;
+
+    Supporter.findOne({email}, (err, supporter)=>{
+        if(err) res.json({result:0, message:"not found supporter"});
+        supporter.img_path = filename;
+        supporter.save(()=>{
+            res.json({result:1, message: "save successful!"});
+        })
+    })
 })
 
 module.exports = router;
