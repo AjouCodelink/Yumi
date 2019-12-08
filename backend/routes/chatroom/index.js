cb783db18a7a26134cf12a4f1819697d1e9eebdb
tkdgusl94 <46221990+tkdgusl94@users.noreply.github.com>
2019-12-08 15:30:43 +0900

    언어교환 백엔드 (#99)
    * chatRoom 스키마 변수 추가
    
    * 방 검색할때 exchanging language인 방 제외
    
    * 방 추천에서도 언어교환방 제외하고 보여주기
    
    * 언어교환 방 검색
    
    * 2명인 언어교환방 검색에서 제거
    
    * 오류 수정
    
    * 쓸데없는 코드 제거

--- backend/routes/chatroom/index.js
+++ backend/routes/chatroom/index.js
@@ -7,6 +7,8 @@ router.use('/list', authMiddleware);
 router.use('/recommend', authMiddleware);
 router.use('/exit/:cr_id', authMiddleware);
 router.use('/entrance/:cr_id', authMiddleware);
+router.use('/exchange-language/creation', authMiddleware);
+router.use('/exchange-language/:language', authMiddleware);
 
 router.get('/search/:keyword', controller.searchWord);
 router.post('/creation', controller.creation);
@@ -16,5 +18,6 @@ router.post('/log', controller.getLog);
 router.get('/participants/:cr_id', controller.getParticipants);
 router.get('/recommend', controller.recommend);
 router.post('/entrance/:cr_id', controller.entrance);
-
+router.post('/exchange-language/creation', controller.exchangeLanCreation);
+router.get('/exchange-language/:language', controller.exchangeLanSearch);
 module.exports = router;
